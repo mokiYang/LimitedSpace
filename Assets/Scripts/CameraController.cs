f4b629e2c8f7bcbcdc1c7e785e5b82264d272680
mokiYang <yangmy2017@gmail.com>
2023-10-01 02:15:45 +0800

    feat: camera controller bugfix

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,29 +4,23 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public Transform player; // 玩家的 Transform
-    public Transform planet; // 行星的 Transform
-    public float distance = 15.0f; // 摄像机距离玩家的距离
+    public Transform player;
+    public Transform planet;
+    public float distance = 15.0f;
+    public float rotationSpeed = 5.0f;
 
     void Update()
     {
-        // 计算从行星到玩家的方向
         Vector3 direction = (player.position - planet.position).normalized;
 
-        // 计算摄像机的位置
         Vector3 cameraPosition = player.position + direction * distance;
 
-        // 将摄像机放置在计算出的位置
         transform.position = cameraPosition;
 
-        // 计算实时的玩家向上向量
-        Vector3 playerUp = (player.position - planet.position).normalized;
+        Vector3 targetForward = (player.position - transform.position).normalized;
 
-        // 使用实时的玩家向上向量计算摄像机应该具有的旋转
-        Quaternion targetRotation = Quaternion.LookRotation(player.position - transform.position, playerUp);
-        Debug.Log("targetRotation:" + targetRotation);
+        Quaternion targetRotation = Quaternion.LookRotation(targetForward, transform.up);
 
-        // 应用旋转
         transform.rotation = targetRotation;
     }
 }
