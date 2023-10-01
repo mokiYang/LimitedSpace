560ac4b00d812f4d4771d2616a9e874eaf92a208
bestdtang <94884915+MinPan9585@users.noreply.github.com>
2023-10-01 20:36:37 +0800

    patrol in a constant speed

--- Assets/Scripts/Min/Patrol.cs
+++ Assets/Scripts/Min/Patrol.cs
@@ -17,6 +17,9 @@ public class Patrol : MonoBehaviour
     [Header("Patrol State")]
     public float patrolRange;
     private Vector3 wayPoint;
+    private Vector3 rotateDir;
+    Quaternion tempRot;
+    float timer = 0f;
 
     private void Start()
     {
@@ -39,15 +42,17 @@ public class Patrol : MonoBehaviour
         switch (enemyState)
         {
             case EnemyStates.PATROL:
-                if(Vector3.Distance(wayPoint, transform.rotation.eulerAngles) <= 1)
+                if(timer/speed >= 0.99f)
                 {
                     GetNewWayPoint();
+                    timer = 0f;
+                    print("aaa");
                 }
                 else
                 {
-                    transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(wayPoint), speed * Time.deltaTime);
-                    print(wayPoint.y);
-                    //rotate to waypoint;
+                    transform.rotation = Quaternion.Lerp(tempRot, Quaternion.Euler(wayPoint), timer / speed);
+                    timer += Time.deltaTime;
+                    //transform.Rotate(rotateDir * speed * Time.deltaTime);
                 }
                 break;
             case EnemyStates.CHASE:
@@ -83,9 +88,13 @@ public class Patrol : MonoBehaviour
     {
         float randomY = Random.Range(startRotation.y - patrolRange, startRotation.y + patrolRange);
         float randomZ = Random.Range(startRotation.z - patrolRange, startRotation.z + patrolRange);
+        print(randomY);
+        print(randomZ);
 
         Vector3 randomRotation = new Vector3(0, randomY, randomZ);
         wayPoint = randomRotation;
+        tempRot = transform.rotation;
+        //rotateDir = wayPoint - transform.rotation.eulerAngles;
     }
 
     private void OnDrawGizmosSelected()
